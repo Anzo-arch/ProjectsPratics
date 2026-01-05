53b9a9a32e3bf8486bf4995f39fc39befd5b96b9
Anzo-arch <andersondimoraes@gmail.com>
2026-01-04 21:05:25 -0300

    Add ANSI color codes to task system output

--- src/projeto_01/Principal.java
+++ src/projeto_01/Principal.java
@@ -3,28 +3,41 @@ package projeto_01;
 import java.util.Scanner;
 
 public class Principal {
+
+    // Códigos ANSI
+    private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
+    private static final String GREEN = "\u001B[32m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String BLUE = "\u001B[34m";
+    private static final String CYAN = "\u001B[36m";
+    private static final String BOLD = "\u001B[1m";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TarefaDiaria tarefaDia = new TarefaDiaria();
 
         while (true) {
-            System.out.println("------------------");
-            System.out.println("Sistema de Tarefas");
-            System.out.println("------------------");
-            System.out.println("1 - Inserir Nova Tarefa");
-            System.out.println("2 - Visualizar Tarefas");
-            System.out.println("3 - Remover Tarefa");
-            System.out.println("4 - Ordenar Tarefas");
-            System.out.println("5 - Sair");
-            System.out.println("------------------");
-            System.out.print("Informe a opção desejada: ");
+
+            System.out.println(BLUE + "------------------" + RESET);
+            System.out.println(BOLD + CYAN + " Sistema de Tarefas" + RESET);
+            System.out.println(BLUE + "------------------" + RESET);
+
+            System.out.println(GREEN + "1 - Inserir Nova Tarefa" + RESET);
+            System.out.println(CYAN + "2 - Visualizar Tarefas" + RESET);
+            System.out.println(YELLOW + "3 - Remover Tarefa" + RESET);
+            System.out.println(BLUE + "4 - Ordenar Tarefas" + RESET);
+            System.out.println(RED + "5 - Sair" + RESET);
+
+            System.out.println(BLUE + "------------------" + RESET);
+            System.out.print(BOLD + "Informe a opção desejada: " + RESET);
 
             int opcao = sc.nextInt();
-            sc.nextLine(); // limpar o buffer
+            sc.nextLine(); // limpar buffer
 
             switch (opcao) {
                 case 1:
-                    System.out.print("Informe uma nova tarefa: ");
+                    System.out.print(CYAN + "Informe uma nova tarefa: " + RESET);
                     String novaTarefa = sc.nextLine();
                     tarefaDia.adicionarTarefa(novaTarefa);
                     break;
@@ -34,7 +47,7 @@ public class Principal {
                     break;
 
                 case 3:
-                    System.out.print("Informe o número da tarefa para remover: ");
+                    System.out.print(YELLOW + "Informe o número da tarefa para remover: " + RESET);
                     int indice = sc.nextInt();
                     sc.nextLine();
                     tarefaDia.removerTarefa(indice - 1);
@@ -45,12 +58,12 @@ public class Principal {
                     break;
 
                 case 5:
-                    System.out.println("Saindo do sistema...");
+                    System.out.println(RED + "Saindo do sistema..." + RESET);
                     sc.close();
                     return;
 
                 default:
-                    System.out.println("Opção inválida!");
+                    System.out.println(RED + "Opção inválida!" + RESET);
                     break;
             }
         }
